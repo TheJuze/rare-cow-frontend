7ec928309d07c40c828e08312d3ea0c85e6617ba
overkam <superivan0755@gmail.com>
2022-04-11 17:27:48 +0300

    vite to cra

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 import { Layout, RouteManager as Router } from 'containers';
 import { ToastContainer } from 'react-toastify';
@@ -7,14 +7,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'assets/styles/index.scss';
 import { WalletConnectContext } from 'services';
 
-const App: FC = () => {
-  return (
-    <WalletConnectContext>
-      <ToastContainer autoClose={4000} hideProgressBar position="top-right" closeButton={false} />
-      <Layout>
-        <Router />
-      </Layout>
-    </WalletConnectContext>
-  );
-};
+const App: FC = () => (
+  <WalletConnectContext>
+    <ToastContainer autoClose={4000} hideProgressBar position="top-right" closeButton={false} />
+    <Layout>
+      <Router />
+    </Layout>
+  </WalletConnectContext>
+);
 export default App;
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useMemo } from 'react';
+import React, { FC, ReactNode, useMemo } from 'react';
 
 import clsx from 'clsx';
 import { omit } from 'lodash';
@@ -16,13 +16,17 @@ export interface ModalProps extends DialogProps {
 export const Modal: FC<ModalProps> = (props) => {
   const { customTitle = ' ', open, children, onClose } = props;
 
-  const jsxTitle = useMemo(() => (typeof customTitle === 'string' ? <div className={clsx(s.title, 'l')}>{customTitle}</div> : customTitle), [customTitle]);
+  const jsxTitle = useMemo(
+    () =>
+      typeof customTitle === 'string' ? (
+        <div className={clsx(s.title, 'l')}>{customTitle}</div>
+      ) : (
+        customTitle
+      ),
+    [customTitle],
+  );
   return (
-    <DialogWrap
-      open={open}
-      onClose={onClose}
-      {...omit({ ...props }, 'customTitle')}
-    >
+    <DialogWrap open={open} onClose={onClose} {...omit({ ...props }, 'customTitle')}>
       <div className={s.modalTitle}>{customTitle && jsxTitle}</div>
       {children}
     </DialogWrap>
