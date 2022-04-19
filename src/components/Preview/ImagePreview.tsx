20819bd9f74c67f7a3dc555fcfd36f642b1659f8
andrey <mastercatpro.tm@gmail.com>
2022-04-19 15:02:25 +0300

    add previewers

---
+++ src/components/Preview/ImagePreview.tsx
@@ -0,0 +1,30 @@
+import React, { VFC } from 'react';
+
+import cn from 'classnames';
+
+import s from './styles.module.scss';
+
+type TImagePreview = {
+  src: string;
+  className?: string;
+  title?: string;
+  alt?: string;
+  cover?: 'cover' | 'contain';
+};
+
+export const ImagePreview: VFC<TImagePreview> = ({
+  src,
+  className,
+  title = '',
+  alt = '',
+  cover = 'cover',
+}) => (
+  <div className={cn(s.imageContainer, className)}>
+    <img
+      src={src}
+      className={cn(s.image, s[cover])}
+      alt={alt || 'preview'}
+      title={title}
+    />
+  </div>
+);
