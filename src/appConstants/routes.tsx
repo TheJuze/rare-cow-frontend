55bfe8b818f0f1af90f806ec0302d09ab139b5f9
overkam <superivan0755@gmail.com>
2022-05-23 20:44:35 +0300

    routes refactor

--- src/appConstants/routes.tsx
+++ src/appConstants/routes.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable max-len */
 /* eslint-disable implicit-arrow-linebreak */
+import { CollectionsList } from 'components';
 import {
   Collection,
   Create,
@@ -14,8 +16,10 @@ import {
   NotFound,
   Profile,
 } from 'pages';
+import { Nfts } from 'pages/Profile/components';
+import Bio from 'pages/Profile/components/Bio';
 import React, { ReactElement } from 'react';
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { TGuards } from 'types';
 
 export type TDynamicValues = { [key: string]: string | number };
@@ -28,7 +32,7 @@ export type TRoutes = {
   label?: string | ReactElement;
   nest?: TNestRoute;
   guards?: TGuards[];
-  inMainTree?: boolean;
+  outlet?: boolean;
 };
 
 const routesConfig = {
@@ -37,57 +41,55 @@ const routesConfig = {
   label: 'Home',
   nest: {
     profile: {
-      path: 'profile/:userId/*',
+      path: 'profile/:userId',
       content: <Profile />,
       label: 'Profile',
+      outlet: true,
       nest: {
         aboutMe: {
           path: 'about-me',
-          content: <Profile />,
+          content: (values) => {
+            console.log('values', values);
+            return <Bio {...values} />;
+          },
           label: 'About',
-          inMainTree: false,
         },
         owned: {
           path: 'owned',
-          content: <Profile />,
+          content: (values) => <Nfts {...values} />,
           label: 'Owned',
-          inMainTree: false,
         },
         forSale: {
           path: 'for-sale',
-          content: <Profile />,
+          content: (values) => <Nfts {...values} />,
           label: 'For sale',
-          inMainTree: false,
         },
         bided: {
           path: 'bided',
-          content: <Profile />,
+          content: (values) => <Nfts {...values} />,
           label: 'Bided',
-          inMainTree: false,
         },
         favorites: {
           path: 'favorites',
-          content: <Profile />,
+          content: (values) => <Nfts {...values} />,
           label: 'Favorites',
-          inMainTree: false,
         },
         collections: {
           path: 'collections',
-          content: <Profile />,
+          content: (values) => <CollectionsList {...values} />,
           label: 'Collections',
-          inMainTree: false,
         },
         sold: {
           path: 'sold',
-          content: <Profile />,
+          content: (values) => <Nfts {...values} />,
           label: 'Sold',
-          inMainTree: false,
         },
         edit: {
           path: 'edit',
           content: <EditProfile />,
           label: 'Edit',
           guards: ['is-me'],
+          outlet: false,
         },
       },
     },
@@ -194,17 +196,9 @@ const getOutletRoute = (nest: TRoutes[]) => {
     const subPath = nest[i];
     if (subPath.nest) {
       return (
-        <Route
-          key={subPath.path}
-          path={subPath.path}
-          element={
-            <>
-              {subPath.content} <Outlet />
-            </>
-          }
-        >
+        <Route key={subPath.path} path={subPath.path} element={<>{subPath.content}</>}>
           {Object.entries(subPath.nest)
-            .map(([, data]) => data)
+            .map(([, data]) => ({ ...data, path: data.path.replaceAll(subPath.path, '').slice(1) }))
             .map((child) => getOutletRoute([child]))}
         </Route>
       );
@@ -214,21 +208,25 @@ const getOutletRoute = (nest: TRoutes[]) => {
   return null;
 };
 
-const recursiveRoutesCollector = (nest: TRoutes[]) => {
+const recursiveRoutesCollector = (nest: TRoutes[], parentOutlet?: boolean) => {
   const resultRoute = [];
+  let hasOutlets = Boolean(parentOutlet ?? nest[0].outlet);
   for (let i = 0; i < nest.length; i += 1) {
     const subPath = nest[i];
     const newPath = subPath.path;
     resultRoute.push(
-      ...(subPath.render !== false && subPath.inMainTree !== false
+      ...(subPath.render !== false &&
+      (subPath.outlet == null ? !hasOutlets : subPath.outlet !== hasOutlets)
         ? [{ ...subPath, path: newPath }]
         : []),
     );
     if (subPath.nest) {
+      hasOutlets = subPath.outlet;
       resultRoute.push([
         ...Object.entries(subPath.nest)
           .map(([, data]) => data)
-          .map((s) => recursiveRoutesCollector([s])),
+          // eslint-disable-next-line no-loop-func
+          .map((s) => recursiveRoutesCollector([s], Boolean(hasOutlets))),
       ]);
     }
   }
@@ -241,7 +239,10 @@ const flattenRoutes = (nest: TRoutes[]) => flatten(recursiveRoutesCollector(nest
 const getRoute = () =>
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   flattenRoutes([routes as any]).map((subPath: TRoutes) => {
-    const { path, content } = subPath;
+    const { path, content, outlet } = subPath;
+    if (outlet) {
+      return getOutletRoute([{ ...subPath }]);
+    }
     return <Route key={path} path={normalizePath(path)} element={content} />;
   });
 
@@ -254,3 +255,4 @@ export const generateOutletRoutes = () => (
   <Routes>{getOutletRoute([{ ...routesConfig }] as any)}</Routes>
 );
 export const generateRoutes = () => <Routes>{getRoute()}</Routes>;
+console.log('generate', generateRoutes());
--- src/pages/Profile/components/Tabs/index.tsx
+++ src/pages/Profile/components/Tabs/index.tsx
@@ -1,11 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/jsx-wrap-multilines */
-import { CollectionsList, Text } from 'components';
 import React, { VFC } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Collection, TokenFull } from 'types/api';
-import Nfts from '../Nfts';
-import styles from './styles.module.scss';
 
 interface ITabsProps {
   bio: string;
@@ -33,104 +30,21 @@ const Tabs: VFC<ITabsProps> = ({
   currentPage,
   collections,
 }) => (
-  <>
-    <Routes>
-      <Route
-        path="about-me"
-        element={
-          <div className={styles.bio}>
-            <Text className={styles.bioTitle} color="dark">
-              Profile Information
-            </Text>
-            <Text className={styles.bioInfo} variant="body-2" color="dark">
-              {bio || 'There is no bio on this profile yet'}
-            </Text>
-          </div>
-        }
-      />
-      <Route
-        path="owned"
-        element={
-          <Nfts
-            setIsShowFilters={setIsShowFilters}
-            isShowChips={isShowChips}
-            isAppliedFilters={isAppliedFilters}
-            appliedFilters={appliedFilters}
-            handleDeleteChips={handleDeleteChips}
-            handleClearChips={handleClearChips}
-            nfts={nfts}
-            onLoadMoreClick={onLoadMoreClick}
-            currentPage={currentPage}
-          />
-        }
-      />
-      <Route
-        path="for-sale"
-        element={
-          <Nfts
-            setIsShowFilters={setIsShowFilters}
-            isShowChips={isShowChips}
-            isAppliedFilters={isAppliedFilters}
-            appliedFilters={appliedFilters}
-            handleDeleteChips={handleDeleteChips}
-            handleClearChips={handleClearChips}
-            nfts={nfts}
-            onLoadMoreClick={onLoadMoreClick}
-            currentPage={currentPage}
-          />
-        }
-      />
-      <Route
-        path="bided"
-        element={
-          <Nfts
-            setIsShowFilters={setIsShowFilters}
-            isShowChips={isShowChips}
-            isAppliedFilters={isAppliedFilters}
-            appliedFilters={appliedFilters}
-            handleDeleteChips={handleDeleteChips}
-            handleClearChips={handleClearChips}
-            nfts={nfts}
-            onLoadMoreClick={onLoadMoreClick}
-            currentPage={currentPage}
-          />
-        }
-      />
-      <Route
-        path="favorites"
-        element={
-          <Nfts
-            setIsShowFilters={setIsShowFilters}
-            isShowChips={isShowChips}
-            isAppliedFilters={isAppliedFilters}
-            appliedFilters={appliedFilters}
-            handleDeleteChips={handleDeleteChips}
-            handleClearChips={handleClearChips}
-            nfts={nfts}
-            onLoadMoreClick={onLoadMoreClick}
-            currentPage={currentPage}
-          />
-        }
-      />
-      <Route path="collections" element={<CollectionsList collections={collections} />} />
-      <Route
-        path="sold"
-        element={
-          <Nfts
-            setIsShowFilters={setIsShowFilters}
-            isShowChips={isShowChips}
-            isAppliedFilters={isAppliedFilters}
-            appliedFilters={appliedFilters}
-            handleDeleteChips={handleDeleteChips}
-            handleClearChips={handleClearChips}
-            nfts={nfts}
-            onLoadMoreClick={onLoadMoreClick}
-            currentPage={currentPage}
-          />
-        }
-      />
-    </Routes>
-  </>
+  <Outlet
+    context={{
+      bio,
+      setIsShowFilters,
+      isShowChips,
+      isAppliedFilters,
+      appliedFilters,
+      handleDeleteChips,
+      handleClearChips,
+      nfts,
+      onLoadMoreClick,
+      currentPage,
+      collections,
+    }}
+  />
 );
 
 export default Tabs;
